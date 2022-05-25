535ff1515bb4e9b8062022f4d02c9675286054a7
Nael1820 <94629276+Nael1820@users.noreply.github.com>
2022-05-25 15:49:27 +0800

    Add files via upload

--- assets/preload/scripts/script.lua
+++ assets/preload/scripts/script.lua
@@ -1,6 +1,6 @@
-function opponentNoteHit()
-    health = getProperty('health')
-    if getProperty('health') > 0.5 then
-        setProperty('health', health- 0.002);
-    end
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'the boy dies'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
 end
